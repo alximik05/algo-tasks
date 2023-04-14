d1e4b90ced1f5d83dea829c2d256c7c68ea3abf2
Stanislav Grishaev <grishaevsd@yandex.ru>
2023-04-14 18:45:09 +0300

    TournamentWinners

---
+++ src/main/java/org/example/TournamentWinners.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TournamentWinners {
+
+    public static void main(String[] args) {
+
+        System.out.println(getWinners(new String[][]{
+                        {"HTML", "C#"},
+                        {"Java", "C++"},
+                        {"Python", "C#"},
+                        {"C++", "C#"}},
+
+                new int[]{
+                        1,
+                        0,
+                        0,
+                        1}
+        ));
+
+    }
+
+//    [["HTML", "C#"],
+//    ["Java", "C++"],
+//    ["Python", "C#"],
+//    ["C++", "C#"]]
+//    [0,1,1,0]
+
+    public static String getWinners(String[][] competitions, int[] result) {
+        Map<String, Integer> resultMap = new HashMap<>();
+        for (int i = 0; i < competitions.length; i++) {
+            String[] competition = competitions[i];
+            int matchResult = result[i];
+            if (matchResult == 1) {
+                if (resultMap.containsKey(competition[0])) {
+                    resultMap.put(competition[0], resultMap.get(competition[0]) + 3);
+                } else {
+                    resultMap.put(competition[0], 3);
+                }
+
+            } else {
+                if (resultMap.containsKey(competition[0])) {
+                    resultMap.put(competition[1], resultMap.get(competition[1]) + 3);
+                } else {
+                    resultMap.put(competition[1], 3);
+                }
+            }
+        }
+
+        String winner = null;
+        for (Map.Entry<String, Integer> stringIntegerEntry : resultMap.entrySet()) {
+            if (winner == null) {
+                winner = stringIntegerEntry.getKey();
+            } else {
+                if (stringIntegerEntry.getValue() > resultMap.get(winner)) {
+                    winner = stringIntegerEntry.getKey();
+                }
+            }
+        }
+        return winner;
+    }
+
+}
